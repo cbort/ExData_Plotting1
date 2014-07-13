a6838beae45855376611833c1bcb8a526e77f67a
Corey Bort <corey.bort@coursesmart.com>
2014-07-12 20:36:24 -0700

    Fixed Plot 1 Title

--- plot1.R
+++ plot1.R
@@ -1,5 +1,5 @@
 # The WD needs to be adjusted for your needs.
-#setwd('D:/Users/cbort/Google Drive/Edu/Coursera/Exploratory Data Analysis/Project1')
+setwd('D:/Users/cbort/Google Drive/Edu/Coursera/Exploratory Data Analysis/Project1')
 dt = read.table("household_power_consumption.txt", sep=";", header=TRUE, stringsAsFactors=FALSE)
 dt$Date <- as.Date(strptime(dt$Date,"%d/%m/%Y"))
 dt$Global_active_power <- as.numeric(dt$Global_active_power)
@@ -8,5 +8,5 @@ dt$Voltage  <- as.numeric(dt$Voltage )
 dt$Global_intensity <- as.numeric(dt$Global_intensity)
 valid<- dt[dt$Date >= as.Date('2007-02-01') & dt$Date <= as.Date('2007-02-02'),]
 png(filename="plot1.png")
-with(valid,hist(Global_active_power, col='red', title='Global Active Power', xlab="Global Active Power (kilowatts)"))
+with(valid,hist(Global_active_power, col='red', main='Global Active Power', xlab="Global Active Power (kilowatts)"))
 dev.off()
